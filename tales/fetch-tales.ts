fd150848be1f66d647f103e64e8ddc79f104a737
Opion <KindledKindred@gmail.com>
2023-08-20 22:07:34 +0900

    chore: vercel.json: functions -> including md

--- tales/fetch-tales.ts
+++ tales/fetch-tales.ts
@@ -6,9 +6,10 @@ import html from "remark-html";
 import { Locale } from "@/i18n/i18n-config";
 import { Tale } from "./tale-type";
 
-const getTalesDirectory = (lang: Locale) => path.join(process.cwd(), `tales/${lang}`);
+const getTalesDirectory = (lang: Locale) =>
+  path.join(process.cwd(), `tales/${lang}`);
 
-async function getTaleByMarkdown (fileName: string, lang: Locale) {
+async function getTaleByMarkdown(fileName: string, lang: Locale) {
   const id = fileName.replace(/\.md$/, "");
   const fullPath = path.join(getTalesDirectory(lang), fileName);
   const fileContents = fs.readFileSync(fullPath, "utf8");
@@ -24,14 +25,16 @@ async function getTaleByMarkdown (fileName: string, lang: Locale) {
     contentHtml,
     ...matterResult.data,
   } as Tale;
-};
+}
 
 export async function getSortedTales(lang: Locale) {
   const fileNames = fs.readdirSync(getTalesDirectory(lang));
-  const allTales = await Promise.all(fileNames.map(async (fileName) => {
-    const tales = await getTaleByMarkdown(fileName, lang)
-    return tales
-  }));
+  const allTales = await Promise.all(
+    fileNames.map(async (fileName) => {
+      const tales = await getTaleByMarkdown(fileName, lang);
+      return tales;
+    })
+  );
   return allTales.sort((a, b) => {
     if (a.date < b.date) {
       return 1;
