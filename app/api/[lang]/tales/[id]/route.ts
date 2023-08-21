8876a660ea54eb5c81d2290c6d492f02a41afd9e
Opion <kindled0kindred@icloud.com>
2023-08-21 14:43:01 +0900

    Revert "たぶんいらないけどapplication/jsonを明示"
    This reverts commit 3f668cc4c2aaac348a7c361a093291fba874c3a6.

--- app/api/[lang]/tales/[id]/route.ts
+++ app/api/[lang]/tales/[id]/route.ts
@@ -15,5 +15,5 @@ export async function GET(
   }
 
   const tale = await getTaleByMarkdown(`${id}.md`, lang);
-  return NextResponse.json({ tale }, { headers: { "content-type": "application/json" }});
+  return NextResponse.json({ tale });
 }
