5937ef0ae40bf19467365ae98628f63b3da8b7c0
Opion <kindled0kindred@icloud.com>
2023-08-21 10:44:49 +0900

    fix: api endpoint

--- app/api/helper.ts
+++ app/api/helper.ts
@@ -2,11 +2,11 @@
  * Next Fetchの第1引数にpathだけを入れたときvercel_urlを補うだけの薄いwrapper
  */
 export function $fetch(url: RequestInfo | URL, init?: RequestInit | undefined) {
-  if (typeof url === "string") {
+  if (typeof url !== "string") {
+    return fetch(url, init);
   }
-  const u =
-    typeof url === "string"
-      ? `${"https://www.monokagari.com/" || "http://localhost:3415/"}${url}`
-      : url;
+  const u = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}${url}`
+      :  `http://localhost:3415/${url}`
   return fetch(u, init);
 }
