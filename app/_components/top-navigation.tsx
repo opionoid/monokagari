5c9599e2427085cb4444ad6546ca6d2255dc6d5f
opionoid <kindled0kindred@icloud.com>
2023-10-04 11:16:40 +0900

    feat: top-navigation bone

--- app/_components/top-navigation.tsx
+++ app/_components/top-navigation.tsx
@@ -1,3 +1,16 @@
+import styles from "./top-navigation.module.css"
+
 export default function TopNavigation() {
-    return <nav><ul></ul></nav>
-}
+  return (
+    <nav className={styles["top-navigation"]}>
+      <ul className={styles["list"]}>
+        <li>
+          <Link href="/trpg">TRPG一覧</Link>
+        </li>
+        <li>
+          <Link href="/mypage">栞</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+};
